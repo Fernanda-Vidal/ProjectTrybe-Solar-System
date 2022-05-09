a9f7bfe916e57be6ca3be54b315a3fe6401c3337
Fernanda Vidal <fevidal.dev@gmail.com>
2022-05-09 18:35:53 -0300

    Corrige lint.

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class PlanetCard extends React.Component {
   render() {
@@ -7,10 +8,15 @@ class PlanetCard extends React.Component {
     return (
       <div data-testid="planet-card">
         <p data-testid="planet-name">{ planetName }</p>
-        <img src={ planetImage } alt={ `Planeta ${planetName}`}></img>
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
       </div>
     );
   }
 }
+
+PlanetCard.propTypes = {
+  planetName: PropTypes.string.isRequired,
+  planetImage: PropTypes.string.isRequired,
+};
 
 export default PlanetCard;
--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -5,7 +5,6 @@ import PlanetCard from './PlanetCard';
 
 class SolarSystem extends React.Component {
   render() {
-    const { name } = this.props;
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
