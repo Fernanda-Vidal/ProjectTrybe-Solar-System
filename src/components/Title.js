bda645142491df4644c5e443a817f0086d42337b
Fernanda Vidal <fevidal.dev@gmail.com>
2022-05-09 17:01:46 -0300

    corrige erro.

---
+++ src/components/Title.js
@@ -0,0 +1,11 @@
+// import React from 'react';
+// import headline from ''
+
+// class Title extends React.Component {
+//   render() {
+//     return (
+//       <h2>{ headline }</h2>
+//     );
+//   }
+// }
+// export default Title;
