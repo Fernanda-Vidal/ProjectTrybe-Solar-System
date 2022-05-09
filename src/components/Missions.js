0e9434f7f1a252b4a753515af68f14da80b616b1
Fernanda Vidal <fevidal.dev@gmail.com>
2022-05-09 20:37:42 -0300

    Adiciona requisito 8.

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Title from './Title';
 
 class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
-        {/* <Missions headline="Missões" /> */}
+        <Title headline="Missões" />
       </div>
     );
   }
